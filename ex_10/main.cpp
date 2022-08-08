f0591cf0d8f62f1eb54a291b1af0ba20617eb9b3
hoivinh20789 <hoivinh20789@gmail.com>
2022-08-08 21:08:58 +0700

    update ex_10

---
+++ ex_10/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/**************************************************
+Viết chương trình in ra màn hình tam giác vuông cân
+đặc có độ cao h. Ví dụ: Nhập h = 4
+*
+* *
+* * *
+* * * *
+****************************************************
+*/
+
+int main() {
+	int side;
+	cout << "Enter side of the isosceles right triangle: ";
+	cin >> side;
+
+	for (int i = 1; i <= side; i++) {
+		for (int p = 1; p <= i; p++) {
+			cout << "* ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
