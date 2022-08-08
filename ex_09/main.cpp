a4dd57936e11d8d180c7d6bb91fa4777b46d830e
hoivinh20789 <hoivinh20789@gmail.com>
2022-08-08 20:58:22 +0700

    update ex_09

---
+++ ex_09/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+Viết chương trình in ra màn hình hình chữ nhật rỗng kích thước m × n.Ví
+dụ : Nhập m = 5; n = 4
+* * * *
+*     *
+*     *
+*     *
+* * * *
+*/
+
+int main() {
+	int vertical;
+	int horizontal;
+	cout << "Enter 2 side of the rectangle: ";
+	cin >> vertical >> horizontal;
+
+	for (int i = 1; i <= vertical; i++) {
+		for (int p = 1; p <= horizontal; p++) {
+			if ((i == 1) || (i == vertical) || (p == 1) || (p == horizontal)) {
+				cout << "* ";
+			}
+			else {
+				cout << "  ";
+			}
+		}
+		cout << '\n';
+	}
+	return 0;
+}
